fe5c190e25a1b8f20ec49d137b1e59a3d2aea78a
BaxtiyorBozorov <baxtiyorbozorov226@gmail.com>
2024-12-10 00:36:12 +0500

    test commit

--- index.js
+++ index.js
@@ -4,4 +4,3 @@ import path, {relative} from 'path'
 
 let fileName = path.join(process.cwd(),'src/common/utils/email.tempilate.html' )
 const result = fs.readFileSync(fileName , "utf-8")
-console.log(result);
